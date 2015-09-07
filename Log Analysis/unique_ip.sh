5eb65ca6129f71e741fbc7966e8780fb8bdcae69
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-09-07 17:39:30 -0500

    continued log analysis

--- Log Analysis/unique_ip.sh
+++ Log Analysis/unique_ip.sh
@@ -20,14 +20,17 @@ echo
 
 echo ${bold} "Largest Export File Size"${normal}
 cat $1|grep POST | awk '{print $6, $10}'
-awk '{ print $10 } ' $1 | sort | uniq |  wc -l
+awk '{ print $10 } ' $1 | sort -k2,2 | uniq |  wc -l
 echo "EO Export "
 echo 
 
 echo ${bold} "Unique Dates"${normal}
 cat $1 | awk '{print $4}' |
-awk -F: '{print $1}' | sort | uniq | wc -l 
+awk -F: '{print $1}' | sort | uniq  | wc -l 
 echo "EO unique dates"
 echo
 
+echo ${bold} "Head and Tails"${normal}
+head -n 1 $1
+tail -n 1 $1
 
